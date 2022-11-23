55db88b49c55aa85e7629a8a8de3480bd6e8f6a6
Flurt <umidrukhiddinov70@gmail.com>
2022-11-23 17:44:50 +0500

    16:40

--- chatty/settings.py
+++ chatty/settings.py
@@ -78,7 +78,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
+            'hosts': [(os.environ.get('POSTGRES_HOST'), 6379)],
         }
     }
 }
@@ -97,16 +97,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': os.environ.get('POSTGRES_DB'),
-        # 'USER': os.environ.get('POSTGRES_USER'),
-        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        # 'HOST': os.environ.get('POSTGRES_HOST'),
-        # 'PORT': os.environ.get('POSTGRES_PORT')
-        'NAME': 'new_db',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT')
+        # 'NAME': 'new_db',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '123',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
